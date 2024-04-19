31133473a99ded9a2f8d29b501be7d39927ee243
anand9773 <aaanand9773@gmail.com>
2024-04-19 15:47:33 +0530

    x-clone.com api developement started (working on configuration)

---
+++ src/main/java/com/api/xclone/XCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.xclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XCloneApplication.class, args);
+	}
+
+}
